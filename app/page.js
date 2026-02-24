25e6dbfe82856d1ac8a66822e0a575e7875df266
JLionPerez <49221382+JLionPerez@users.noreply.github.com>
2026-02-24 14:18:29 -0800

    Fetch volume title, authors, description, and thumbnails.
    I'm thinking of turning this into a search engine for books.
    I might just start with searching by title or authors,
    then move to including more features like categories (epub or not),
    and if it comes with a free download or something. For now
    just a handy search engine to browse books available in the library.
    Seeing descriptions for them here and there.

--- app/page.js
+++ app/page.js
@@ -1,22 +1,24 @@
 "use client";
-import styles from "./page.module.css";
 import { useState, useEffect } from "react";
 
 export default function Home() {
   const key = "AIzaSyAAAc5MboaBXYWpD0mkYIPQfRNUDlGP43A"
-  const url = `https://www.googleapis.com/books/v1/volumes?q=intitle:flowers+inauthor:keyes&key=${key}`
+  const url = `https://www.googleapis.com/books/v1/volumes?q=all&printType=books&key=${key}`
   const [books, setBooks] = useState([])
 
   useEffect(() => {
-    const response = fetch(url).then(response => response.json()).then(data => setBooks(data.items));
-    // console.log(books)
+    const response = fetch(url)
+    .then(response => response.json())
+    .then(data => setBooks(data.items)) //console.log(data)
   },[])
 
   return (
     <>
       <div>{books.map(book => (
         <div key={book.id}>
-          {book.volumeInfo.title} by {book.volumeInfo.authors}
+          <img src={book.volumeInfo.imageLinks.thumbnail}></img>
+          <p id="name">{book.volumeInfo.title} by {book.volumeInfo.authors}</p>
+          <p id="text">{book.volumeInfo.description}</p>
         </div>
       ))}</div>
     </>
